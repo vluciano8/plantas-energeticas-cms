7d775e931f7d5d64c3d5a1ac9e00925a7cf852c4
Your Name <you@example.com>
2020-10-15 13:59:55 +0200

    added `gatsby-plugin-purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,8 @@ module.exports = {
     `gatsby-transformer-json`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-catch-links`,
-    
+    `gatsby-plugin-preact`,
+    `gatsby-plugin-netlify`,    
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
@@ -78,8 +79,18 @@ module.exports = {
         icon: `static/favicon.ico`,
       },
     },
-    `gatsby-plugin-preact`,
-    `gatsby-plugin-netlify`,
+    { 
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    }
+   
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
